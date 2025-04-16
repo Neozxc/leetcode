d81cbbddcd505112c821cd1bbb505693b14c3172
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com.>
2025-04-16 10:50:53 +0100

    2537

---
+++ 2537. Count the number of Good Subarrays.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        freq = defaultdict(int)
+        total_pairs = 0
+        count = 0
+        answer = 0
+
+        for right in range(len(nums)):
+            num = nums[right]
+
+            total_pairs += freq[num]
+            freq[num] += 1
+
+            while total_pairs >= k:
+                answer += len(nums) - right
+
+                freq[nums[left]] -= 1
+                total_pairs -= freq[nums[left]]
+                left += 1
+
+        return answer
