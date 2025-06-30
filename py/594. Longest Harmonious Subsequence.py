fb852e7703e141d6fdf4002db0406ac6d136bbec
Ned Sakalauskis <neduska991@gmail.com>
2025-06-30 15:00:55 +0100

    594 (#45)
    * .
    
    * .
    
    * .

---
+++ py/594. Longest Harmonious Subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        x = Counter(nums)
+        y = 0
+
+        for num in nums:
+            if num + 1 in x:
+                c = x[num] + x[num + 1]
+                y = max(y, c)
+
+        return y
