8e8dd3810c89f033fe4d09196660cd96aedd8351
Ned Sakalauskis <neduska991@gmail.com>
2025-07-23 10:51:08 +0100

    . (#61)

---
+++ py/1717. Maximum Score From Removing Substrings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumGain(sel, s: str, x: int, y: int) -> int:
+        def removePairs(pair, score):
+            nonlocal s
+            res = 0
+            stack = []
+
+            for c in s:
+                if c == pair[1] and stack and stack[-1] == pair[0]:
+                    stack.pop()
+                    res += score
+                else:
+                    stack.append(c)
+
+            s = "".join(stack)
+
+            return res
+
+
+        res = 0
+        pair = "ab" if x > y else "ba"
+        res += removePairs(pair, max(x, y))
+        res += removePairs(pair[::-1], min(x, y))
+
+        return res
