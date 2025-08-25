4808c7d9670122b3752d52f0941cb43a4a48cbda
Ned Sakalauskis <neduska991@gmail.com>
2025-08-25 17:09:00 +0100

    . (#83)

---
+++ py/498. Diagonal Traverse.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        if not mat or not mat[0]:
+            return []
+
+        m, n = len(mat), len(mat[0])
+        result = [0] * (m * n)
+        row, col = 0, 0
+        going_up = True
+
+        for i in range(m * n):
+            result[i] = mat[row][col]
+
+            if going_up:
+                if col == n - 1:
+                    row += 1
+                    going_up = False
+                elif row == 0:
+                    col += 1
+                    going_up = False
+                else:
+                    row -= 1
+                    col += 1
+            else:
+                if row == m - 1:
+                    col += 1
+                    going_up = True
+                elif col == 0:
+                    row += 1
+                    going_up = True
+                else:
+                    row += 1
+                    col -= 1
+        
+        return result
